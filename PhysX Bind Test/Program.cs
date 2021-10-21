deed2d3f795d9976f5c1e932bd2dd5820b9829a1
SUNNY <husnainsd@gmail.com>
2021-10-22 04:17:27 +0500

    Add project files.

---
+++ PhysX Bind Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace PhysX_Bind_Test
+{
+    class Program
+    {
+
+        [DllImport(@"\HelperDlls\PhysXDevice64.dll", EntryPoint = "physxDevInit")]
+        private static extern int physxDevInit();
+
+
+        [DllImport(@"\HelperDlls\PhysXDevice64.dll", EntryPoint = "physxDevErrorString", CharSet = CharSet.Unicode)]
+        private static extern int physxDevErrorString(ref string message);
+
+        static void Main(string[] args)
+        {
+
+            var init = physxDevInit();
+            string err = null;
+            init = physxDevErrorString(ref err);
+            Console.WriteLine("Hello World!");
+            Console.Read();
+
+        }
+    }
+}
